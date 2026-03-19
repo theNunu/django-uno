62225eeb1d3870efe9fff54bfad393396fc33a17
Alexis Cepeda <alexkiller0408@hotmail.com>
2026-03-18 23:59:00 -0500

    ya voy

--- Aplicaciones/Academico/urls.py
+++ Aplicaciones/Academico/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.home)
+    path("", views.home),
+    path("/<str:codigo>/", views.getByCodigo, name="CursoDetalle"),
 ]
--- Aplicaciones/Academico/views.py
+++ Aplicaciones/Academico/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import Curso
 # Create your views here.
-
+from django.shortcuts import render, get_object_or_404
 def home(request):
     cursos = Curso.objects.all()
     print("los cursos: ",cursos)
     return render(request, "gestionCursos.html", {"cursos": cursos})
+
+
+def getByCodigo(request, codigo):
+    # Obtener el objeto por su clave primaria (pk) o ID
+    objeto = get_object_or_404(Curso, pk=codigo)
+    print("detalle de un producto: ",objeto)
+    #    objeto = get_object_or_404(Cu0so, codigo=codigo) 
+    return render(request, 'cursoDetalle.html', {'item': objeto})
