93a09ccbfdec336c6f752adc5c646dbe1fdde690
Alexis Cepeda <alexkiller0408@hotmail.com>
2026-03-18 22:27:39 -0500

    consumiendo mas cosas

--- Aplicaciones/Academico/views.py
+++ Aplicaciones/Academico/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import Curso
 # Create your views here.
 
 def home(request):
-    # print(request)
-    return render(request, "gestionCursos.html")
+    cursos = Curso.objects.all()
+    print("los cursos: ",cursos)
+    return render(request, "gestionCursos.html", {"cursos": cursos})
